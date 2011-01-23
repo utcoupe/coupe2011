7198778c26d684352a78048841e435d1a62a403f
Arthur Puyou <arthur@puyou.fr>
2011-01-24 00:10:12 +0100

    Réglage des Kx_ALPHA pour pas d'oscillations en ligne droite

--- motorControl/parameters.h
+++ motorControl/parameters.h
@@ -58,11 +58,11 @@
 #define KI_ANGLE 5.0
 #define KD_ANGLE 1.0
 #define KP_DELTA 2.0
-#define KI_DELTA 5.0
+#define KI_DELTA 0.0
 #define KD_DELTA 1.0
-#define KP_ALPHA 2.0
-#define KI_ALPHA 5.0
-#define KD_ALPHA 1.0
+#define KP_ALPHA .05
+#define KI_ALPHA 0.0
+#define KD_ALPHA 55.0
 
 /*Paramètre de liaison série*/
 #define SOF '<'
