c253199835df8d833fb5e47568f26508ec2da8c5
Matthieu Guffroy <mattgu74@gmail.com>
2011-01-24 20:07:35 +0100

    supression de constante

--- com/carte/message.cpp
+++ com/carte/message.cpp
@@ -55,30 +55,6 @@ void analyzeMessage(int bufferIndex, int* buffer){
 	
 	// On analyse le message en fonction de son type
 	switch(buffer[0]){
-		/* A gérer :
-		#define T_PAUSE 4
-		#define T_RESET 6
-		#define T_ALIGN 7
-		#define T_SETX 9
-		#define T_SETY 10
-		*/
-		case T_QUERY:
-                        Serial.print(SOF);
-			Serial.print(robot_state.x, DEC);
-			Serial.print(robot_state.y, DEC);
-			Serial.print(robot_state.angle, DEC);
-			//Serial.print(checksum);
-                        Serial.print(EOF);
-		break;
-		case T_POSITION:
-			//pushGoal(TYPE_POSITION, buffer[2] << 8 + buffer[3], buffer[3] << 8 + buffer[4]);
-		break;
-		case T_SPEED:
-			//pushGoal(TYPE_SPEED, buffer[2], 0);
-		break;
-		case T_ANGLE:
-			//pushGoal(TYPE_ANGLE, buffer[2] << 8 + buffer[3], 0);
-		break;
 		case '?':
 			Serial.println("_______HELP__________");
 			Serial.print("time: ");Serial.println(millis());
