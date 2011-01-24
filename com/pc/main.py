1e0a76b902fd6d712badb34d066d05ff1f6ec862
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2011-01-24 15:24:14 +0100

    threading des connections

--- com/pc/main.py
+++ com/pc/main.py
@@ -2,22 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import serial
+import threading
+import time
+
+ser = dict()
 
 def connect(port):
+	global ser
 	for i in range(10):
 		try:
-			ser = serial.Serial('/dev/tty'+port, 115200)
+			#print 'tentative de connection sur %s'%port
+			s = serial.Serial('/dev/tty'+port, 115200)
 		except serial.SerialException:
 			print 'connection echouée sur %s, nouvelle tentative'%port
 		else:
 			print 'connection %s établie'%port
-			return ser
+			ser[port] = s
 	print 'echec de la connection %s après 10 tentatives, relancer le programme'%port
-	return False
+	exit()
 
-ser0 = connect('ACM0')
-ser1 = connect('ACM1')
-if ser0:
-	print ser0.readline()
-if ser1:
-	print ser1.readline()
+
+a = threading.Thread(None, connect, None, ('ACM0',))
+b = threading.Thread(None, connect, None, ('ACM1',))
+a.start()
+b.start()
+
+while a.isAlive() or b.isAlive():
+	time.sleep(1)
+
+print ser['ACM0'].readline()
+print ser['ACM1'].readline()
+ser['ACM1'].write('<?>\n')
+for i in range(50):
+	print ser['ACM1'].readline()
+
+
