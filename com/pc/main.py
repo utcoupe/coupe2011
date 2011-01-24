144ca80cc8d5552bdc7f26e33b393562cb3b30b4
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2011-01-24 18:00:36 +0100

    amélioration de la fonction connect avec des timeout et un paramètre pour le refresh-rate

--- com/pc/main.py
+++ com/pc/main.py
@@ -7,12 +7,12 @@ import time
 
 ser = dict()
 
-def connect(port):
+def connect(port, refresh):
 	global ser
 	for i in range(10):
 		try:
 			#print 'tentative de connection sur %s'%port
-			s = serial.Serial('/dev/tty'+port, 115200, timeout=1, writeTimeout=1)
+			s = serial.Serial('/dev/tty'+port, refresh, timeout=1, writeTimeout=1)
 		except serial.SerialException:
 			print 'connection echouée sur %s, nouvelle tentative'%port
 		else:
@@ -23,36 +23,50 @@ def connect(port):
 	exit()
 
 
-a = threading.Thread(None, connect, None, ('ACM0',))
-b = threading.Thread(None, connect, None, ('ACM1',))
+a = threading.Thread(None, connect, None, ('ACM0',9600,))
+#b = threading.Thread(None, connect, None, ('ACM1',115200))
 a.start()
-b.start()
+#b.start()
 
-while a.isAlive() or b.isAlive():
+while a.isAlive():# or b.isAlive():
 	time.sleep(1)
 
 time.sleep(2)
 
-v = input()
-
-ser['ACM0'].write(chr(v))
-print 'ACM0 : '+ser['ACM0'].readline()
-#print ser['ACM1'].readline()
-ser['ACM1'].write('<?>')
-#ser['ACM1'].write('<?>')
-#ser['ACM1'].write('<?>')
-for i in range(10):
-	print 'ACM1 : '+ser['ACM1'].readline()
+while True:
+	ser['ACM0'].write(chr(1))
+	val = ser['ACM0'].readline()
+	if val:
+		print 'ACM0 : '+val
+	else:
+		print 'timeout'
 	
+"""
+tot = 0
+for i in xrange(300):
+	t = time.time()
+	ser['ACM0'].write(chr(1))
+	val = ser['ACM0'].readline()
+	if val:
+		print 'ACM0 : '+val
+		t = time.time()-t
+		print t
+		tot += t
+	else:
+		print 'timeout'
+print tot
+print tot/300.0
 
-
-ser['ACM0'].write(chr(v))
-print 'ACM0 : '+ser['ACM0'].readline()
-#print ser['ACM1'].readline()
-ser['ACM1'].write('<?>')
-#ser['ACM1'].write('<?>')
-#ser['ACM1'].write('<?>')
-for i in range(10):
-	print 'ACM1 : '+ser['ACM1'].readline()
-
+tot = 0
+for i in xrange(300):
+	t = time.time()
+	ser['ACM1'].write('<?>')
+	for i in range(8):
+		print 'ACM1 : '+ser['ACM1'].readline()
+	t = time.time()-t
+	print t
+	tot += t
+print tot
+print tot/300.0
+"""
 
