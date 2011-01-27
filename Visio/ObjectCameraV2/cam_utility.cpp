a98d0f6b91a40f21fe4ae2d9243c154a85418329
xrodrigu <xavier.rodriguez@hds.utc.fr>
2011-01-27 23:07:10 +0100

    Grosse mise à jour
    - Communication intelligence (envoyer 1 pour que la cam vous réponde)
    -detection des pionts
    -renvoie des distances et des "angles" x' y' lol
    
    et pis plein d'autre truc je me rappelle plus
    
    Signed-off-by: xrodrigu <xavier.rodriguez@hds.utc.fr>

---
+++ Visio/ObjectCameraV2/cam_utility.cpp
@@ -0,0 +1,23 @@
+/**
+*****************************************************
+    Copyright (c)
+    RODRIGUEZ   Xavier          xavier.rodriguez@hds.utc.fr
+
+    cam_utility.cpp
+    UTCoupe 2010-2011
+    All rights reserved.
+
+*   Ensemble de fonctions simples mais pratiques
+*****************************************************
+ **/
+#include "cam_utility.hpp"
+/****************************************************
+*****              FONCTIONS                    *****
+****************************************************/
+string intToString(int entier)
+{
+    ostringstream out;
+    out << entier;
+    return out.str();
+}
+
---
+++ Visio/ObjectCameraV2/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+#include "cv.h"
+#include "highgui.h"
+
+#include "UTCamera.hpp"
+
+#define NOM_FENETRE "CamCaptor"
+
+
+using namespace std;
+
+
+#include "cam_regionAnalyser.hpp"
+
+void* activCamera(void* pData)
+{
+    int id = (int)pData;
+    UTCamera *cam = new UTCamera(id, 1280, 800);
+    cam->generalRun();
+    //cam->calibrage();
+}
+
+int main()
+{
+
+    // ***************
+    /*
+    pthread_t cameraAvant;
+    pthread_t cameraArriere;
+
+    pthrais bon c'est pas grave on fera la vidéo demain au réveil (13-14h) à ce qu'il parrait les vidéos sont plus réussi à la lumiére du jour... ;)
+
+Bonne fin de nuit les petits...ead_create(&cameraAvant  , NULL, activCamera, (void *)CAMERA_AVA);
+    pthread_create(&cameraArriere, NULL, activCamera, (void *)CAMERA_ARR);
+
+    pthread_join (cameraAvant, NULL);
+    pthread_join (cameraArriere, NULL);
+    */
+    // ***************
+
+
+    //fenetreDeParametrageDesCourbes(CAMERA_AVA);
+
+   // parametrage(CAMERA_ARR);
+
+    //double a, b, c;
+    /*computeCoef(&a, &b, &c,
+                 230, 480, 490, 240, 1640, 0);
+
+*/
+    //UTCamera *ctour à la ligneamAv = new UTCamera(CAMERA_TEST, 1280, 800);
+
+    UTCamera *camAv = new UTCamera(CAMERA_AVA, 1280, 800);
+
+    //camAv->calibrage();
+    camAv->generalRun();
+
+
+
+    //camAv->record("vtest.avi",VISION_NORMAL);
+
+    return 0;
+}
+
+
+
