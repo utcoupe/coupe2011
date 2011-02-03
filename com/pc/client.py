27ca88bb1f1f73b4d31faf73641e7ec57ef19f55
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2011-02-03 22:02:37 +0100

    oublié d'envoyer ce fichier...

---
+++ com/pc/client.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+
+class ClientSerial():
+	def __init__(self, serial, origin, baudrate):
+		self._serial = serial
+		self.origin = origin
+		self.baudrate = baudrate
+		
+	def write(self, msg):
+		self._serial.write(msg)
+		
+	def readline(self):
+		return self._serial.readline()
+	
+	def __str__(self):
+		return "ClientSerial(%s)"%self.origin
+	
+	def close(self):
+		self._serial.close()
+
+
+class ClientSubprocess():
+	def __init__(self, subprocess, origin, baudrate):
+		self._subprocess = subprocess
+		self.origin = origin
+		self.baudrate = baudrate
+	
+	def write(self, msg):
+		self._subprocess.stdin.write(str(msg)+"\n") # envoie au child
+		self._subprocess.stdin.flush()
+	
+	def readline(self):
+		return self._subprocess.stdout.readline()
+	
+	def __str__(self):
+		return "ClientSubprocess(%s)"%self.origin
+	
+	def close(self):
+		self._subprocess.kill()
