08fc7d18dc588b78c0b95c818b57576c34181e6c
clefebvr <clefebvr@etu.utc.fr>
2011-02-20 22:23:08 +0100

    précisions dans les commentaires de moduloPI()
    Signed-off-by: clefebvr <clefebvr@etu.utc.fr>

--- motorControl/control.cpp
+++ motorControl/control.cpp
@@ -485,6 +485,9 @@ void computeRobotState(){
  * moduloPi( 3*PI/2 )= - PI/2
  * moduloPi( PI ) = PI
  * moduloPi( 0 ) = 0
+ *
+ * x-y*((int)(x/y)) => c'est le modulo pour des doubles (x%y)
+ * trunc et int c'est pareil au final, j'ai choisi trunc juste pour que ce soit plus complique !
  * */
 double moduloPI(double Nb){
 	double result;
