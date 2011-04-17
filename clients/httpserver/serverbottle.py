892b78acf42bdcf6ba095ee01e2839ae2a278cbe
CC <cc@cc-laptop.(none)>
2011-04-17 20:03:41 +0200

    Visio code barre

--- clients/httpserver/serverbottle.py
+++ clients/httpserver/serverbottle.py
@@ -15,8 +15,8 @@ import socket
 
 HOST = 'localhost'
 PORT = 50000
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((HOST, PORT))
 
 # -----------------------------
 # API
