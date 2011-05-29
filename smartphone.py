73349e8c8a8af2adda30d5dde678606fc7e39ca6
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2011-05-29 22:38:18 +0200

    pour lancer rapidement le code de vlad

---
+++ smartphone.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+ROOT_DIR  = os.path.dirname(os.path.abspath(__file__))
+
+import sys
+import subprocess
+
+if len(sys.argv) > 1:
+	p = subprocess.Popen(["java","-cp",os.path.join(ROOT_DIR,"Visio","Smartphone","bin"),"Main"])
+else:
+	p = subprocess.Popen(["java","-cp",os.path.join(ROOT_DIR,"Visio","Smartphone","bin"),"Main","noview"])
+	
+
+p.wait()
