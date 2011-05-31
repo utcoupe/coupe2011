d93db6f0cb73ff97f8d4cc417d9079b8700104be
pierreh37 <pierreh37@gmail.com>
2011-05-31 22:27:43 +0200

    ascenseur

--- pinceControl/carte/encoder.cpp
+++ pinceControl/carte/encoder.cpp
@@ -1,61 +1,87 @@
-/*
- * encoder.cpp
- *
- *  Created on: 13 janv. 2011
- *      Author: HoHen
- */
 #include "encoder.h"
 
-int state_left_pinA;
-int state_left_pinB;
-int state_right_pinA;
-int state_right_pinB;
+int state_AR_pinA;
+int state_AR_pinB;
+int state_AV_pinA;
+int state_AV_pinB;
 
-long value_left_enc;//AR
-long value_right_enc;//AV
+long value_AR_enc;//AR left
+long value_AV_enc;//AV right
 long goal_position_AV,goal_position_AR;
+
+void arretMsgAV(){
+	setAVPWM(0x00);
+	if(msg_position_AV!=-1){
+		sendMessage(msg_position_AV,2);
+		msg_position_AV=-1;
+	}
+}
+
+void arretMsgAR(){
+	setARPWM(0x00);
+	if(msg_position_AR!=-1){
+		sendMessage(msg_position_AR,2);
+		msg_position_AR=-1;
+	}
+}
+
 
 void testAV()
 {
 	if (goal_position_AV < 0)
 		return;
-	if (value_right_enc < goal_position_AV-MARGE_MARCHE)
+	if (value_AV_enc < goal_position_AV-MARGE_MARCHE)
 		setAVPWM(PWM_MOVE);
-	else if (value_right_enc < goal_position_AV-MARGE_MAINTIENT)
+	else if (value_AV_enc < goal_position_AV-MARGE_MAINTIENT)
 		setAVPWM(PWM_MAINTIENT);
-	else if (value_right_enc > goal_position_AV+MARGE_MARCHE)
+	else if (value_AV_enc > goal_position_AV+MARGE_MARCHE)
 		setAVPWM(-PWM_MOVE);
-	else if (value_right_enc > goal_position_AV+MARGE_MAINTIENT)
+	else if (value_AV_enc > goal_position_AV+MARGE_MAINTIENT)
 		setAVPWM(-PWM_MAINTIENT);
 	else //arret
+		arretMsgAV();
+		
+	if (digitalRead(PIN_MS_RECAL_AV_HAUT) == HIGH)
 	{
-		setAVPWM(0x00);
-		if(msg_position_AV!=-1){
-			sendMessage(msg_position_AV,2);
-			msg_position_AV=-1;
-		}
+		value_AV_enc = POSITION_MAX;
+		//sendMessage(-101, (char*)"ar haut");
+		//arretMsgAV();
+
+	}else if (digitalRead(PIN_MS_RECAL_AV_BAS) == HIGH)
+	{
+		value_AV_enc = 0;
+		//sendMessage(-101, (char*)"ar haut");
+		//arretMsgAV();
 	}
+		
 }
 
 void testAR()
 {
 	if (goal_position_AR < 0)
 		return;
-	if (value_left_enc < goal_position_AR-MARGE_MARCHE)
+	if (value_AR_enc < goal_position_AR-MARGE_MARCHE)
 		setARPWM(PWM_MOVE);
-	else if (value_left_enc < goal_position_AR-MARGE_MAINTIENT)
+	else if (value_AR_enc < goal_position_AR-MARGE_MAINTIENT)
 		setARPWM(PWM_MAINTIENT);
-	else if (value_left_enc > goal_position_AR+MARGE_MARCHE)
+	else if (value_AR_enc > goal_position_AR+MARGE_MARCHE)
 		setARPWM(-PWM_MOVE);
-	else if (value_left_enc > goal_position_AR+MARGE_MAINTIENT)
+	else if (value_AR_enc > goal_position_AR+MARGE_MAINTIENT)
 		setARPWM(-PWM_MAINTIENT);
 	else //arret
+		arretMsgAR();
+		
+	if (digitalRead(PIN_MS_RECAL_AR_HAUT) == HIGH)
 	{
-		setARPWM(0x00);
-		if(msg_position_AR!=-1){
-			sendMessage(msg_position_AR,2);
-			msg_position_AR=-1;
-		}
+		value_AR_enc = POSITION_MAX;
+		//sendMessage(-101, (char*)"ar haut");
+		//arretMsgAR();
+
+	}else if (digitalRead(PIN_MS_RECAL_AR_BAS) == HIGH)
+	{
+		value_AR_enc = 0;
+		//sendMessage(-101, (char*)"ar haut");
+		//arretMsgAR();
 	}
 }
 
@@ -69,10 +95,10 @@ void encoderSafe()
 	//static int last_right_enc_value = 0;
 	
 	if (goal_position_AR >= 0
-		and ((value_left_enc < goal_position_AR-MARGE_MAINTIENT)
-		or (value_left_enc > goal_position_AR+MARGE_MAINTIENT)))
+		and ((value_AR_enc < goal_position_AR-MARGE_MAINTIENT)
+		or (value_AR_enc > goal_position_AR+MARGE_MAINTIENT)))
 	{
-		if (abs(last_left_enc_value - value_left_enc) < 10)
+		if (abs(last_left_enc_value - value_AR_enc) < 10)
 		{
 			goal_position_AV = -1;
 			setARPWM(0x00);
@@ -81,10 +107,10 @@ void encoderSafe()
 	}*/
 	
 	/*if (goal_position_AV >= 0
-		and ((value_right_enc < goal_position_AV-MARGE_MAINTIENT)
-		or (value_right_enc > goal_position_AV+MARGE_MAINTIENT)))
+		and ((value_AV_enc < goal_position_AV-MARGE_MAINTIENT)
+		or (value_AV_enc > goal_position_AV+MARGE_MAINTIENT)))
 	{
-		if (abs(last_right_enc_value - value_right_enc) < 10)
+		if (abs(last_right_enc_value - value_AV_enc) < 10)
 		{
 			goal_position_AR = -1;
 			setAVPWM(0x00);
@@ -94,117 +120,122 @@ void encoderSafe()
 }
 
 void initEncoders(){
-	value_left_enc = 0;
-	value_right_enc = 0;
+	value_AR_enc = 0;
+	value_AV_enc = 0;
 
-	pinMode(PIN_LEFT_A,INPUT);
-	pinMode(PIN_LEFT_B,INPUT);
-	pinMode(PIN_RIGHT_A,INPUT);
-	pinMode(PIN_RIGHT_B,INPUT);
+	pinMode(PIN_AR_A,INPUT);
+	pinMode(PIN_AR_B,INPUT);
+	pinMode(PIN_AV_A,INPUT);
+	pinMode(PIN_AV_B,INPUT);
+	
+	pinMode(PIN_MS_RECAL_AV_HAUT,INPUT);
+	pinMode(PIN_MS_RECAL_AV_BAS,INPUT);
+	pinMode(PIN_MS_RECAL_AR_HAUT,INPUT);
+	pinMode(PIN_MS_RECAL_AR_BAS,INPUT);
 
-	digitalWrite(PIN_LEFT_A, HIGH);
-	digitalWrite(PIN_LEFT_B, HIGH);
-	digitalWrite(PIN_RIGHT_A, HIGH);
-	digitalWrite(PIN_RIGHT_B, HIGH);
+	digitalWrite(PIN_AR_A, HIGH);
+	digitalWrite(PIN_AR_B, HIGH);
+	digitalWrite(PIN_AV_A, HIGH);
+	digitalWrite(PIN_AV_B, HIGH);
 
-	state_left_pinA = digitalRead(PIN_LEFT_A);
-	state_left_pinB = digitalRead(PIN_LEFT_B);
-	state_right_pinA = digitalRead(PIN_RIGHT_A);
-	state_right_pinB = digitalRead(PIN_RIGHT_B);
+	state_AR_pinA = digitalRead(PIN_AR_A);
+	state_AR_pinB = digitalRead(PIN_AR_B);
+	state_AV_pinA = digitalRead(PIN_AV_A);
+	state_AV_pinB = digitalRead(PIN_AV_B);
 
-	//attachInterrupt(INTERRUPT_LEFT_A,valueChangeOnEncoderLeftPinA,CHANGE);
-	//attachInterrupt(INTERRUPT_LEFT_B,valueChangeOnEncoderLeftPinB,CHANGE);
-	//attachInterrupt(INTERRUPT_RIGHT_A,valueChangeOnEncoderRightPinA,CHANGE);
-	//attachInterrupt(INTERRUPT_RIGHT_B,valueChangeOnEncoderRightPinB,CHANGE);
-	attachInterrupt(INTERRUPT_LEFT_A,valueChangeOnMSRecalARHaut,CHANGE);
-	attachInterrupt(INTERRUPT_LEFT_B,valueChangeOnMSRecalARBas,CHANGE);
+	attachInterrupt(INTERRUPT_AR_A,valueChangeOnEncoderARPinA,CHANGE);
+	attachInterrupt(INTERRUPT_AV_B,valueChangeOnEncoderARPinB,CHANGE);
+	attachInterrupt(INTERRUPT_AV_A,valueChangeOnEncoderAVPinA,CHANGE);
+	attachInterrupt(INTERRUPT_AV_B,valueChangeOnEncoderAVPinB,CHANGE);
+	/*attachInterrupt(INTERRUPT_LEFT_A,valueChangeOnMSRecalARHaut,CHANGE);
+	attachInterrupt(INTERRUPT_AV_B,valueChangeOnMSRecalARBas,CHANGE);
 	attachInterrupt(INTERRUPT_RIGHT_A,valueChangeOnMSRecalAVHaut,CHANGE);
-	attachInterrupt(INTERRUPT_RIGHT_B,valueChangeOnMSRecalAVBas,CHANGE);
+	attachInterrupt(INTERRUPT_RIGHT_B,valueChangeOnMSRecalAVBas,CHANGE);*/
 }
 
 
-void valueChangeOnEncoderLeftPinA(){
-	int new_state = digitalRead(PIN_LEFT_A);
+void valueChangeOnEncoderARPinA(){
+	int new_state = digitalRead(PIN_AR_A);
 
 	if(new_state == HIGH)
-		if(state_left_pinB == HIGH)
-			value_left_enc--;
+		if(state_AR_pinB == HIGH)
+			value_AR_enc--;
 		else
-			value_left_enc++;
+			value_AR_enc++;
 
 	else
-		if(state_left_pinB == HIGH)
-			value_left_enc++;
+		if(state_AR_pinB == HIGH)
+			value_AR_enc++;
 		else
-			value_left_enc--;
+			value_AR_enc--;
 
-	state_left_pinA = new_state;
+	state_AR_pinA = new_state;
 	testAR();
 }
 
-void valueChangeOnEncoderLeftPinB(){
-	int new_state = digitalRead(PIN_LEFT_B);
+void valueChangeOnEncoderARPinB(){
+	int new_state = digitalRead(PIN_AR_B);
 
 	if(new_state == HIGH)
-		if(state_left_pinA == HIGH)
-			value_left_enc++;
+		if(state_AR_pinA == HIGH)
+			value_AR_enc++;
 		else
-			value_left_enc--;
+			value_AR_enc--;
 
 	else
-		if(state_left_pinA == HIGH)
-			value_left_enc--;
+		if(state_AR_pinA == HIGH)
+			value_AR_enc--;
 		else
-			value_left_enc++;
+			value_AR_enc++;
 
-	state_left_pinB = new_state;
+	state_AR_pinB = new_state;
 }
 
-void valueChangeOnEncoderRightPinA(){
-	int new_state = digitalRead(PIN_RIGHT_A);
+void valueChangeOnEncoderAVPinA(){
+	int new_state = digitalRead(PIN_AV_A);
 
 	if(new_state == HIGH)
-		if(state_right_pinB == HIGH)
-			value_right_enc--;
+		if(state_AV_pinB == HIGH)
+			value_AV_enc--;
 		else
-			value_right_enc++;
+			value_AV_enc++;
 
 	else
-		if(state_right_pinB == HIGH)
-			value_right_enc++;
+		if(state_AV_pinB == HIGH)
+			value_AV_enc++;
 		else
-			value_right_enc--;
+			value_AV_enc--;
 
-	state_right_pinA = new_state;
+	state_AV_pinA = new_state;
 	testAV();
 }
 
-void valueChangeOnEncoderRightPinB(){
-	int new_state = digitalRead(PIN_RIGHT_B);
+void valueChangeOnEncoderAVPinB(){
+	int new_state = digitalRead(PIN_AV_B);
 
 	if(new_state == HIGH)
-		if(state_right_pinA == HIGH)
-			value_right_enc++;
+		if(state_AV_pinA == HIGH)
+			value_AV_enc++;
 		else
-			value_right_enc--;
+			value_AV_enc--;
 
 	else
-		if(state_right_pinA == HIGH)
-			value_right_enc--;
+		if(state_AV_pinA == HIGH)
+			value_AV_enc--;
 		else
-			value_right_enc++;
+			value_AV_enc++;
 
-	state_right_pinB = new_state;
+	state_AV_pinB = new_state;
 }
 
 
-void valueChangeOnMSRecalAVBas()
+/*void valueChangeOnMSRecalAVBas()
 {
 	delay(500);
 	
 	if (digitalRead(PIN_MS_RECAL_AV_BAS) == HIGH)
 	{
-		value_right_enc = 0;
+		value_AV_enc = 0;
 		sendMessage(-101, (char*)"av bas");
 		setAVPWM(0x00);
 		if(msg_position_AV != -1){
@@ -221,7 +252,7 @@ void valueChangeOnMSRecalAVHaut()
 	
 	if (digitalRead(PIN_MS_RECAL_AV_HAUT) == HIGH)
 	{
-		value_right_enc = POSITION_MAX;
+		value_AV_enc = POSITION_MAX;
 		sendMessage(-101, (char*)"av haut");
 		setAVPWM(0x00);
 		if(msg_position_AV != -1){
@@ -238,7 +269,7 @@ void valueChangeOnMSRecalARBas()
 	
 	if (digitalRead(PIN_MS_RECAL_AR_BAS) == HIGH)
 	{
-		value_left_enc = 0;
+		value_AR_enc = 0;
 		sendMessage(-101, (char*)"ar bas");
 		setARPWM(0x00);
 		if(msg_position_AR != -1){
@@ -255,7 +286,7 @@ void valueChangeOnMSRecalARHaut()
 	
 	if (digitalRead(PIN_MS_RECAL_AR_HAUT) == HIGH)
 	{
-		value_left_enc = POSITION_MAX;
+		value_AR_enc = POSITION_MAX;
 		sendMessage(-101, (char*)"ar haut");
 		setARPWM(0x00);
 		if(msg_position_AR != -1){
@@ -263,4 +294,4 @@ void valueChangeOnMSRecalARHaut()
 			msg_position_AR=-1;
 		}
 	}
-}
+}*/
