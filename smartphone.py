b6e0b8a77dddabdd1e96cc46d3fe4785cc66663c
Vlad <cc@cc-Latitude-E6400.(none)>
2011-06-01 03:32:10 +0200

    nouveaux arguments pour smartphone.py

--- smartphone.py
+++ smartphone.py
@@ -7,10 +7,14 @@ ROOT_DIR  = os.path.dirname(os.path.abspath(__file__))
 import sys
 import subprocess
 
-if len(sys.argv) > 1:
-	p = subprocess.Popen(["java","-cp",os.path.join(ROOT_DIR,"Visio","Smartphone","bin"),"Main"])
-else:
-	p = subprocess.Popen(["java","-cp",os.path.join(ROOT_DIR,"Visio","Smartphone","bin"),"Main","noview"])
+#if len(sys.argv) > 1:
+#	p = subprocess.Popen(["java","-cp",os.path.join(ROOT_DIR,"Visio","Smartphone","bin"),"Main"])
+#else:
+#	p = subprocess.Popen(["java","-cp",os.path.join(ROOT_DIR,"Visio","Smartphone","bin"),"Main","noview"])
+
+
+# ===> usage: noview/view ip_serveur_python ip_smartphone
+p = subprocess.Popen(["java","-cp",os.path.join(ROOT_DIR,"Visio","Smartphone","bin"),"Main","noview", "10.42.43.1", "192.168.43.1"]);
 	
 
 p.wait()
